ea1d60790bd115838bb854814fa6fc0fd61e8f93
Kevin Finisterre <kf@digitalmunition.com>
2014-03-26 01:38:20 -0400

    add en-rGB fix minor issue in merge.sh rename en-419 to en-rES

--- merge.sh
+++ merge.sh
@@ -1,5 +1,5 @@
 if [ ! -d "../OpenPilot/" ]; then
-	git clone git://git.openpilot.org/OpenPilot.git 
+	git clone git://git.openpilot.org/OpenPilot.git ../OpenPilot
 fi
 cp -rfv ./* ../OpenPilot/
 cd ../OpenPilot
